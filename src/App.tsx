c22763c97e27d9f1c5713337bdf4b572aa517f86
Zenaida19 <yovanizenaida@gmail.com>
2025-05-02 08:46:57 -0500

    rama zenaida

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
-
+  console.log(count);
+  
   return (
     <>
       <div>
@@ -14,6 +16,7 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+
         </a>
       </div>
       <h1>Vite + React</h1>
